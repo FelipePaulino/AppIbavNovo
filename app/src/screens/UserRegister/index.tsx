4d39b0546a43d27b572d1879bc02e3d59b18e60c
Felipe Paulino <felipepaulino08@hotmail.com>
2023-01-13 15:25:06 -0300

    ajustando valor do campo de estado

--- app/src/screens/UserRegister/index.tsx
+++ app/src/screens/UserRegister/index.tsx
@@ -160,7 +160,6 @@ export function UserRegisterScreen() {
     credentialsPost();
   };
 
-
   const credentialsPost = () => {
     const dataLider = {
       bairro: address.bairro,
@@ -185,7 +184,7 @@ export function UserRegisterScreen() {
             nome: formValues.name,
             bairro: address.bairro,
             email: formValues.email,
-            estado: address.uf ? address.uf : formValues.state,
+            estado: formValues.state ? formValues.state : '',
             rede: formValues.network,
             cidade: address.localidade,
             senha: formValues.password,
