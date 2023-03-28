7a9b873ab813a183dc51e18ea61e02be2b9e8ad6
Felipe Paulino <felipepaulino08@hotmail.com>
2023-03-28 08:58:37 -0300

    mudando banco

--- app/src/common/services/ConnectApi/index.tsx
+++ app/src/common/services/ConnectApi/index.tsx
@@ -1,5 +1,10 @@
 import axios from "axios";
+//HOMOLOGAÇÃO
+const api = 'https://app-homologacao-a6b59-default-rtdb.firebaseio.com'
+
+//PROD
+// const api = https://app-ibav-f06f4-default-rtdb.firebaseio.com
 
 export const connectApi = axios.create({
-  baseURL: "https://app-ibav-f06f4-default-rtdb.firebaseio.com",
+  baseURL: api,
 });
