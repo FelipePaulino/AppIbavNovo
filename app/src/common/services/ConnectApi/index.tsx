40e60794fdb66348aea6692528f85b582f7192be
Felipe Paulino <felipepaulino08@hotmail.com>
2023-06-03 16:44:07 -0300

    change database

--- app/src/common/services/ConnectApi/index.tsx
+++ app/src/common/services/ConnectApi/index.tsx
@@ -1,9 +1,9 @@
 import axios from "axios";
 //HOMOLOGAÇÃO
-const api = 'https://app-homologacao-a6b59-default-rtdb.firebaseio.com'
+// const api = 'https://app-homologacao-a6b59-default-rtdb.firebaseio.com'
 
 //PROD
-//const api = 'https://app-ibav-f06f4-default-rtdb.firebaseio.com'
+const api = 'https://app-ibav-f06f4-default-rtdb.firebaseio.com'
 
 export const connectApi = axios.create({
   baseURL: api,
