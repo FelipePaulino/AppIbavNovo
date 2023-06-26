afeb9d8e83b2caf9335f99e3b41818f8c6ec073e
Felipe Paulino <felipepaulino08@hotmail.com>
2023-06-26 13:17:17 -0300

    force update

--- app/src/routes/index.tsx
+++ app/src/routes/index.tsx
@@ -1,13 +1,40 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 
 import { AppRoutes } from "./AppStack/index.routes";
 import { AuthRouter } from "./AuthStack/index.routes";
 
 import { useAuth } from "../hooks/useAuth";
+import { Alert, BackHandler, Linking } from "react-native";
+import VersionCheck from "react-native-version-check";
 
 export const Routes = () => {
   const { user } = useAuth();
+  useEffect(() => {
+    checkUpdateNeeded();
+  }, []);
+
+  const checkUpdateNeeded = async () => {
+    try {
+      let updateNeeded = await VersionCheck.needUpdate();
+      if (updateNeeded && updateNeeded.isNeeded) {
+        Alert.alert(
+          "Atualize o Aplicativo",
+          "Você precisa atualizar seu aplicativo",
+          [
+            {
+              text: "Atualizar",
+              onPress: () => {
+                BackHandler.exitApp();
+                Linking.openURL(updateNeeded.storeUrl);
+              },
+            },
+          ],
+          { cancelable: false }
+        );
+      }
+    } catch (error) {}
+  };
 
   return (
     <NavigationContainer>
