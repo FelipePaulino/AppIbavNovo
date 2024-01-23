99280cc8c1139d1edf7c04d83d3e0eb26e8f74aa
Robson Andrade <robson.andrade19@gmail.com>
2024-01-23 09:25:58 -0300

    feat: add configuration for api endpoint

--- app/src/config/firebase.ts
+++ app/src/config/firebase.ts
@@ -1,17 +1,11 @@
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+import { API_ENDPOINT } from "./apiConfig";
 
-import { initializeApp } from 'firebase/app';
-import { getStorage } from 'firebase/storage';
-
-//HOMOLOGAÇÃO
-const api = 'https://app-homologacao-a6b59-default-rtdb.firebaseio.com'
-
-//PROD
-//const api = 'https://app-ibav-f06f4-default-rtdb.firebaseio.com'
-
- const firebaseConfig2 = {
+const firebaseConfig2 = {
   apiKey: "AIzaSyDtmBnzDYs3MPshWh65eH-8XuUue2Mtk7Y",
   authDomain: "app-ibav-f06f4.firebaseapp.com",
-  databaseURL: api,
+  databaseURL: API_ENDPOINT,
   projectId: "app-ibav-f06f4",
   storageBucket: "app-ibav-f06f4.appspot.com",
   messagingSenderId: "924421424445",
@@ -21,16 +15,13 @@ const api = 'https://app-homologacao-a6b59-default-rtdb.firebaseio.com'
 export const firebaseConfig = {
   apiKey: "AIzaSyDtmBnzDYs3MPshWh65eH-8XuUue2Mtk7Y",
   authDomain: "app-ibav-f06f4.firebaseapp.com",
-  databaseURL: api,
+  databaseURL: API_ENDPOINT,
   projectId: "app-ibav-f06f4",
   storageBucket: "app-ibav-f06f4.appspot.com",
   messagingSenderId: "924421424445",
   appId: "1:924421424445:web:2079d39c0e428380dd3154",
 };
 
-export const app = initializeApp(firebaseConfig2)
+export const app = initializeApp(firebaseConfig2);
 
-export const storage = getStorage(app)
-
-;
-
+export const storage = getStorage(app);
