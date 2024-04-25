92da2021ba19607587628c1fec1829572d813ad1
felipe.ribeiro <felipe.ribeiro@globalsys.com.br>
2024-04-25 16:45:02 -0300

    mudanca

--- app/src/common/services/ConnectApi/index.tsx
+++ app/src/common/services/ConnectApi/index.tsx
@@ -1,5 +1,6 @@
 import axios from "axios";
-import { API_ENDPOINT } from "../../../config/api";
+import { API_ENDPOINT } from "../../../config/base"
+
 export const connectApi = axios.create({
   baseURL: API_ENDPOINT,
 });
--- app/src/config/api/index.tsx
+++
@@ -1,4 +0,0 @@
-
-export const API_ENDPOINT = "https://app-ibav-f06f4-default-rtdb.firebaseio.com";
-
-
